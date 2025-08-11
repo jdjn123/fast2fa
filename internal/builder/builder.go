2263f95b6d7dc6cfddf4dc39e721596c5706c043
Administrator <gitlab_admin_c2c9ae@example.com>
2025-08-11 16:22:05 +0800

    25-8-11

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -1,32 +1,26 @@
 package builder
 
 import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "os/exec"
-    "path/filepath"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
 )
 
+// BuildTargetBinary 替换 SECRET 占位符，并编译为 setup2fa
 func BuildTargetBinary(secret string) error {
-    // 读取 target.go 模板
-    targetCode, err := ioutil.ReadFile(filepath.Join("target", "target.go"))
-    if err != nil {
-        return err
-    }
+	// 替换 SECRET 占位符
+	code := fmt.Sprintf(string(TargetGo), secret)
 
-    // 替换 SECRET 占位符
-    code := fmt.Sprintf(string(targetCode), secret)
+	// 写入临时 main.go
+	if err := ioutil.WriteFile("main.go", []byte(code), 0644); err != nil {
+		return err
+	}
+	defer os.Remove("main.go")
 
-    // 写入临时 main.go
-    if err := ioutil.WriteFile("main.go", []byte(code), 0644); err != nil {
-        return err
-    }
-    defer os.Remove("main.go")
-
-    // 编译为 setup2fa
-    cmd := exec.Command("go", "build", "-o", "setup2fa", "main.go")
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    return cmd.Run()
+	// 编译为 setup2fa
+	cmd := exec.Command("go", "build", "-o", "setup2fa", "main.go")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
